ae3c671487d1c5a92c09b6d7c70240efbbf4b981
FabricioLimaProgramador <fabriciolimaprogramador@gmail.com>
2020-06-14 00:28:16 -0300

    update junit

--- src/test/java/repository/UserRepositoryTests.java
+++ src/test/java/repository/UserRepositoryTests.java
@@ -2,14 +2,17 @@ package repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.springcourse.domain.User;
 import com.springcourse.domain.enums.Role;
 import com.springcourse.repository.UserRepository;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserRepositoryTests {
 
