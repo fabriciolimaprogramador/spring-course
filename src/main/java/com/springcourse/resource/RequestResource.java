052694d6543410164e6fe06c1fce5069aa50a71d
FabricioLimaProgramador <fabriciolimaprogramador@gmail.com>
2020-06-21 20:09:55 -0300

    Create request stage resource

--- src/main/java/com/springcourse/resource/RequestResource.java
+++ src/main/java/com/springcourse/resource/RequestResource.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
 import com.springcourse.service.RequestService;
+import com.springcourse.service.RequestStageService;
 
 @RestController
 @RequestMapping(value = "requests")
@@ -22,6 +24,9 @@ public class RequestResource {
 
 	@Autowired
 	private RequestService requestService;
+
+	@Autowired
+	private RequestStageService stageService;
 
 	@PostMapping
 	public ResponseEntity<Request> save(@RequestBody Request request) {
@@ -46,6 +51,12 @@ public class RequestResource {
 	public ResponseEntity<List<Request>> listAll() {
 		List<Request> requests = requestService.listAll();
 		return ResponseEntity.ok(requests);
+	}
+
+	@GetMapping("/{id}/request-stages")
+	public ResponseEntity<List<RequestStage>> listAllStagesById(@PathVariable(name = "id") Long id) {
+		List<RequestStage> stages = stageService.listAllByRequestId(id);
+		return ResponseEntity.ok(stages);
 	}
 
 }
