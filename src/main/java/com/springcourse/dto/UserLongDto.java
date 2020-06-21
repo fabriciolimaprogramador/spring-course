003ed996d9eb90d02bc2c7613862720bfbc85ed8
FabricioLimaProgramador <fabriciolimaprogramador@gmail.com>
2020-06-21 19:54:29 -0300

    Request resource

--- src/main/java/com/springcourse/dto/UserLongDto.java
+++ src/main/java/com/springcourse/dto/UserLongDto.java
@@ -1,5 +1,18 @@
 package com.springcourse.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
 public class UserLongDto {
 
+	private String email;
+	private String password;
+	
+	
+	
 }
--- src/main/java/com/springcourse/resource/UserResource.java
+++ src/main/java/com/springcourse/resource/UserResource.java
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springcourse.domain.Request;
 import com.springcourse.domain.User;
 import com.springcourse.dto.UserLongDto;
+import com.springcourse.service.RequestService;
 import com.springcourse.service.UserService;
 
 @RestController
@@ -23,6 +25,10 @@ public class UserResource {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private RequestService requestService;
+	
 
 	@PostMapping
 	public ResponseEntity<User> save(@RequestBody User user) {
@@ -54,5 +60,13 @@ public class UserResource {
 		User loggedUser = userService.login(user.getEmail(), user.getPassword());
 		return ResponseEntity.ok(loggedUser);
 	}
+	
+	@GetMapping("/{id}/requests")
+	public ResponseEntity<List<Request>> listAllRequestById(@PathVariable(name = "id") Long id){
+		List<Request> requests = requestService.listAllByOwnerId(id);
+		return ResponseEntity.ok(requests);
+	}
+	
+	
 
 }
